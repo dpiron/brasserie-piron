6cfd154c56cb49c910849c681b207f2465cc2580
dpiron <den.piron@gmail.com>
2022-07-10 01:45:01 +0200

    saveurs

--- main.py
+++ main.py
@@ -883,7 +883,7 @@ def order():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=False)
 
 # TODO : Ecrire page d'info, formulaire contact
 # TODO : Commander : renvoie vers formulaire contact?
